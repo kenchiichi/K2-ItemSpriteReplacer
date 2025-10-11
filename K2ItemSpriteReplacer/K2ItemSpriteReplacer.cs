7e207667ea97d2c53298df95a65cb0e95a22b493
kenchiichi <kenshinii@outlook.com>
2025-10-11 17:35:14 -0600

    v1

--- K2ItemSpriteReplacer/K2ItemSpriteReplacer.cs
+++ K2ItemSpriteReplacer/K2ItemSpriteReplacer.cs
@@ -1,39 +1,75 @@
-﻿using Asuna.Dialogues;
+﻿using ANToolkit.Controllers;
+using Asuna.CharManagement;
+using Asuna.Dialogues;
+using Asuna.Items;
 using Modding;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
 using UnityEngine;
 
 namespace K2ItemSpriteReplacer
 {
     public class TCModExample : ITCMod
     {
-        public void OnDialogueStarted(Dialogue dialogue)
-        {
-            Debug.Log("Modded OnDialogueStarted");
-        }
+        public void OnDialogueStarted(Dialogue dialogue) { }
 
-        public void OnFrame(float deltaTime)
-        {
+        public void OnFrame(float deltaTime) { }
 
-        }
+        public void OnLevelChanged(string oldLevel, string newLevel) { }
 
-        public void OnLevelChanged(string oldLevel, string newLevel)
-        {
-            Debug.Log("Modded OnLevelChanged");
-        }
-
-        public void OnLineStarted(DialogueLine line)
-        {
-            Debug.Log("Modded OnLineStarted");
-        }
+        public void OnLineStarted(DialogueLine line) { }
 
         public void OnModLoaded(ModManifest manifest)
         {
-            Debug.Log("Modded OnModLoaded");
+            Debug.Log("K2-ItemSpriteReplacer Installed");
+
+            using StreamReader k2StreamReader = new StreamReader(Path.Combine(manifest.ModPath, "data\\ItemData.xml"));
+
+            List<ItemSpriteInfo> k2Sprites = Deserialize<List<ItemSpriteInfo>>(k2StreamReader.ReadToEnd());
+
+            Item.OnItemCloned.AddListener((newItem, oldItem) =>
+            {
+                if (newItem is Equipment && oldItem is Equipment)
+                {
+                    foreach (var item in k2Sprites)
+                    {
+                        if (oldItem.Name == item.Name)
+                        {
+                            Equipment clone = newItem as Equipment;
+                            clone.DisplaySpriteResource = manifest.SpriteResolver.ResolveAsResource(Path.Combine(manifest.ModPath, item.PreviewSpritePath));
+                            clone.DurabilityDisplayLayers.ForEach(layer =>
+                            {
+                                layer.DurabilitySprites.IntactResource = manifest.SpriteResolver.ResolveAsResource(Path.Combine(manifest.ModPath, item.IntactResourcePath));
+                                layer.DurabilitySprites.RuinedResource = manifest.SpriteResolver.ResolveAsResource(Path.Combine(manifest.ModPath, item.DamagedResourcePath));
+                                layer.DurabilitySprites.DamagedResource = manifest.SpriteResolver.ResolveAsResource(Path.Combine(manifest.ModPath, item.RuinedResourcePath));
+                                layer.DurabilitySprites.DestroyedResource = manifest.SpriteResolver.ResolveAsResource(Path.Combine(manifest.ModPath, item.DestroyedResourcePath));
+                            });
+                        }
+                    }
+                }
+            });
         }
 
         public void OnModUnLoaded()
         {
-            Debug.Log("Modded OnModUnLoaded");
+            Debug.Log("K2-ItemSpriteReplacer Uninstalled");
         }
+        private static T Deserialize<T>(string xmlString)
+        {
+            if (xmlString == null) return default;
+            var serializer = new XmlSerializer(typeof(T));
+            using var reader = new StringReader(xmlString);
+            return (T)serializer.Deserialize(reader);
+        }
+    }
+    public class ItemSpriteInfo
+    {
+        public string Name { get; set; }
+        public string PreviewSpritePath { get; set; }
+        public string IntactResourcePath { get; set; }
+        public string DamagedResourcePath { get; set; }
+        public string RuinedResourcePath { get; set; }
+        public string DestroyedResourcePath { get; set; }
     }
 }
