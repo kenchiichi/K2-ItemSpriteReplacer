9bd5bbd7e85112abee20705b0a703d7ba43ae90d
kenchiichi <kenshinii@outlook.com>
2025-10-11 15:24:06 -0600

    init

---
+++ K2ItemSpriteReplacer/K2ItemSpriteReplacer.cs
@@ -0,0 +1,39 @@
+﻿using Asuna.Dialogues;
+using Modding;
+using UnityEngine;
+
+namespace K2ItemSpriteReplacer
+{
+    public class TCModExample : ITCMod
+    {
+        public void OnDialogueStarted(Dialogue dialogue)
+        {
+            Debug.Log("Modded OnDialogueStarted");
+        }
+
+        public void OnFrame(float deltaTime)
+        {
+
+        }
+
+        public void OnLevelChanged(string oldLevel, string newLevel)
+        {
+            Debug.Log("Modded OnLevelChanged");
+        }
+
+        public void OnLineStarted(DialogueLine line)
+        {
+            Debug.Log("Modded OnLineStarted");
+        }
+
+        public void OnModLoaded(ModManifest manifest)
+        {
+            Debug.Log("Modded OnModLoaded");
+        }
+
+        public void OnModUnLoaded()
+        {
+            Debug.Log("Modded OnModUnLoaded");
+        }
+    }
+}
